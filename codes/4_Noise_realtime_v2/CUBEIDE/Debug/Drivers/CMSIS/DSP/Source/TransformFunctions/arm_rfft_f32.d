54edf1f2ff2eb3429928986e46eda67a185992e6
emreoztoklu <emreoztoklu@hotmail.com>
2022-03-22 12:15:53 +0300

    Initial commit
    DSP Examples ARM

--- codes/4_Noise_realtime_v2/CUBEIDE/Debug/Drivers/CMSIS/DSP/Source/TransformFunctions/arm_rfft_f32.d
+++ codes/4_Noise_realtime_v2/CUBEIDE/Debug/Drivers/CMSIS/DSP/Source/TransformFunctions/arm_rfft_f32.d
@@ -1,12 +1,17 @@
 Drivers/CMSIS/DSP/Source/TransformFunctions/arm_rfft_f32.o: \
  ../Drivers/CMSIS/DSP/Source/TransformFunctions/arm_rfft_f32.c \
- F:/DSP/DSP_ARM/codes/Noise_realtime/CUBEIDE/Drivers/CMSIS/DSP/Include/arm_math.h \
+ F:/DSP/DSP_ARM/DSP_STM32F4/codes/4_Noise_realtime_v2/CUBEIDE/Drivers/CMSIS/DSP/Include/arm_math.h \
  ../Drivers/CMSIS/Include/core_cm4.h \
  ../Drivers/CMSIS/Include/cmsis_version.h \
  ../Drivers/CMSIS/Include/cmsis_compiler.h \
  ../Drivers/CMSIS/Include/cmsis_gcc.h
-F:/DSP/DSP_ARM/codes/Noise_realtime/CUBEIDE/Drivers/CMSIS/DSP/Include/arm_math.h:
+
+F:/DSP/DSP_ARM/DSP_STM32F4/codes/4_Noise_realtime_v2/CUBEIDE/Drivers/CMSIS/DSP/Include/arm_math.h:
+
 ../Drivers/CMSIS/Include/core_cm4.h:
+
 ../Drivers/CMSIS/Include/cmsis_version.h:
+
 ../Drivers/CMSIS/Include/cmsis_compiler.h:
+
 ../Drivers/CMSIS/Include/cmsis_gcc.h:
