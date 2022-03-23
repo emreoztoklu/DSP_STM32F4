bd482d4b7d1dab70c523e803e25b39cd71758913
emreoztoklu <emreoztoklu@hotmail.com>
2022-03-23 17:30:48 +0300

    Initial commit
    DSP Examples ARM

--- codes/11_First_Difference/user/main.c
+++ codes/11_First_Difference/user/main.c
@@ -7,15 +7,21 @@
 
 /*********************************************/
 /*extern DATA TYPE */
-extern float32_t InputSignal_f32_1kHz_15kHz[SIGNAL_LENGHT];
+extern float32_t InputSignal_f32_1kHz_15kHz[SIGNAL_LENGHT];     // Time Domain Signal
 float32_t *inputSignal = &InputSignal_f32_1kHz_15kHz[0];
 
 /*DATA TYPE */
 uint32_t freq;
 float32_t inputSample;
 float32_t outputSample;
+
 float_t output_signal_arr[SIGNAL_LENGHT];
 
+/*for DFT solution*/
+float_t ReX[SIGNAL_LENGHT/2];
+float_t ImX[SIGNAL_LENGHT/2];
+float32_t ReX_signal;
+float32_t ImX_signal;
 
 /*********************************************/
 /*FUNCTION PROTOTYPES */
@@ -26,11 +32,16 @@ void plot_both_signal(void);
 
 void plot_signal(float32_t * src_signal, uint32_t sig_lenght, float32_t *sample);
 
-
+/*Calculate Running Sum*/
 void calc_running_sum(float32_t *sig_src_arr, float32_t *sig_dest_arr, uint32_t sig_lenght);
 
+/*Calculate First difference*/
 void calc_first_difference(float32_t *sig_src_arr, float32_t *sig_dest_arr, uint32_t sig_lenght);
-	
+
+/*Calculate DFT of signal*/
+void calc_sig_DFT(float32_t *sig_src_arr, float32_t *sig_dest_rex_arr, float32_t *sig_dest_imx_arr, uint32_t sig_lenght);
+void get_DFT_output_Magnitude(void);
+void plot_rex_signal(void);
 
 /*********************************************/
 
@@ -39,13 +50,17 @@ int main(void){
 	SystemClock_Config();
 	freq = HAL_RCC_GetHCLKFreq();
 	
-  calc_first_difference((float32_t*)&inputSignal[0], (float32_t*)&output_signal_arr[0], (uint32_t)SIGNAL_LENGHT);
-	plot_both_signal();
-	
+//  calc_first_difference((float32_t*)&inputSignal[0], (float32_t*)&output_signal_arr[0], (uint32_t)SIGNAL_LENGHT);
+//	plot_both_signal();
+		
+	calc_sig_DFT((float32_t*)&inputSignal[0], (float32_t*)&ReX[0], (float32_t*)&ImX[0], (uint32_t)SIGNAL_LENGHT);
+	get_DFT_output_Magnitude();
+	plot_rex_signal();	
+
+
 	while(1){	
 	
 	}	
-
 }
 
 /*running sound smooth and filter ses yumusatma */
@@ -59,12 +74,10 @@ void calc_running_sum(float32_t *sig_src_arr, float32_t *sig_dest_arr, uint32_t 
 		//y[n] = x[n] + y[n-1]  running sum
 		sig_dest_arr[i] = sig_src_arr[i] +  sig_dest_arr[i-1];
 	}
-
 }
 
 
 void calc_first_difference(float32_t *sig_src_arr, float32_t *sig_dest_arr, uint32_t sig_lenght){
-	
 	int i ;
 	
 	sig_dest_arr[0]= 0;
@@ -75,6 +88,43 @@ void calc_first_difference(float32_t *sig_src_arr, float32_t *sig_dest_arr, uint
 			sig_dest_arr[i] = sig_src_arr[i]-sig_src_arr[i-1];
 	}
 }	
+
+void calc_sig_DFT(float32_t *sig_src_arr, float32_t *sig_dest_rex_arr, float32_t *sig_dest_imx_arr, uint32_t sig_lenght){
+	int i, j, k;
+	
+	for( j = 0; j < sig_lenght/2; j++){
+		sig_dest_rex_arr[j] = 0;
+		sig_dest_imx_arr[j] = 0;
+	}
+	
+	for(k=0;k < sig_lenght/2; k++){
+		for(i=0; i < sig_lenght; i++){
+			sig_dest_rex_arr[k] += sig_src_arr[i] * cos(2*PI*k*i/sig_lenght); 
+			sig_dest_imx_arr[k] -= sig_src_arr[i] * sin(2*PI*k*i/sig_lenght);
+		}
+	}
+}
+
+void get_DFT_output_Magnitude(void){
+	int k;
+	
+	for(k = 0; k < SIGNAL_LENGHT/2; k++){
+		ReX[k] = fabs(ReX[k]);
+	}
+
+}
+
+
+
+void plot_rex_signal(void){
+	int i, j;
+	for(i = 0; i < SIGNAL_LENGHT/2; i++){
+		ReX_signal =  ReX[i];
+		for(j = 0; j < 6000; j++)
+		;
+	}
+}
+
 
 void plot_both_signal(void){
 	int i, j;
